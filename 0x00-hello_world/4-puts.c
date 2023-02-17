cd9242033acaab0b3a56dd3cff4a44da28a02d2a
ojistheleft <ojinyamartha@gmail.com>
2023-02-16 16:54:04 -0800

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,3 @@
-#!/bin/bash
-vi 4-puts.c
 #include <stdio.h>
 /**
 *main - Entry point
@@ -11,4 +9,3 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
-
